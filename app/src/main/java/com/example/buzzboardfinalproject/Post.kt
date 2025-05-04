072eddf1fd6bb11e46858273fb2ec4a9abd553a9
DKing0518 <kingdmiyal@gmail.com>
2025-05-04 17:44:44 -0400

    Homepage Updates

---
+++ app/src/main/java/com/example/buzzboardfinalproject/Post.kt
@@ -0,0 +1,9 @@
+package com.example.buzzboardfinalproject
+
+data class Post (
+    val username: String,
+    val title: String,
+    val description: String,
+    val location: String,
+    val imageResId: Int
+)
