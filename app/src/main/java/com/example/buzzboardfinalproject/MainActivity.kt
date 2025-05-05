faebb87a2da5752c76ed44588077ad371aa4356b
brunoonwuazor <Brunoaustin07@gmail.com>
2025-05-05 15:09:06 -0400

    Post tab

--- app/src/main/java/com/example/buzzboardfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/buzzboardfinalproject/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.example.buzzboardfinalproject
 
 import android.os.Bundle
@@ -5,35 +6,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.example.buzzboardfinalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-
-        // Setup insets
+        replaceFragment(HomeFragment())
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Setup NavController with BottomNavigationView
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
 
-        binding.bottomNavigationView.setupWithNavController(navController)
+                    R.id.home -> replaceFragment(HomeFragment())
+                R.id.favorites -> replaceFragment(FavoritesFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.add_post -> replaceFragment(AddPostFragment())
+                R.id.messages -> replaceFragment(MessagesFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
 
